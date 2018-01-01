05150e3cd8324b833d3356294b755ad6d27ac21e
mcboat <mcboat@harakirimail.com>
2018-01-01 16:04:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Discord reaction bot
+A simple Discord reaction bot created originally for @coltonrawr#5011.
+
+## Usage
+1. `npm i`
+2. Modify `config.json`
+3. `node index.js`
+4. Profit!
---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const fs = require('fs')
+const path = require('path')
+
+const config = JSON.parse(fs.readFileSync(
+  path.resolve(__dirname, 'config.json')
+).toString())
+const client = new Discord.client()
+
+client.on('message', msg => {
+  for (let i of config.reactions) {
+    let regex = new RegExp(
+      i.regex.content,
+      i.regex.global ? 'g' : '' +
+      i.regex.multiline ? 'm' : '' +
+      i.regex.caseInsensitive ? 'i' : ''
+    )
+    if (regex.test(msg.content)) {
+      msg.react(
+        i.serverEmoji ?
+          client.emojis.find('name', i.emoji) :
+          `:${i.emoji}:`
+      )
+    }
+  }
+})
+
+client.login(config.token)
