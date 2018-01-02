31e477d24ad3603ffe94c8cef221f3cd1d5bd435
mcboat <mcboat@harakirimail.com>
2018-01-02 15:26:32 -0500

    Fixed bugs

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const path = require('path')
 const config = JSON.parse(fs.readFileSync(
   path.resolve(__dirname, 'config.json')
 ).toString())
-const client = new Discord.client()
+const client = new Discord.Client()
 
 client.on('message', msg => {
   for (let i of config.reactions) {
     let regex = new RegExp(
       i.regex.content,
-      i.regex.global ? 'g' : '' +
-      i.regex.multiline ? 'm' : '' +
-      i.regex.caseInsensitive ? 'i' : ''
+      (i.regex.global ? 'g' : '') +
+      (i.regex.multiline ? 'm' : '') +
+      (i.regex.caseInsensitive ? 'i' : '')
     )
     if (regex.test(msg.content)) {
       msg.react(
         i.serverEmoji ?
-          client.emojis.find('name', i.emoji) :
-          `:${i.emoji}:`
+          client.emojis.find('name', i.serverEmoji) :
+          i.unicodeEmoji
       )
     }
   }
